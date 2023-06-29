214fba0c4e63a1a4e0e02fd4bdeaf6828058dfeb
yogeshvakhre05 <=yogeshvakhre05@gmail.com>
2023-06-29 12:22:21 +0530

    Added About section in home page

--- src/views/pages/Home/Home.js
+++ src/views/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import BannerSection from "../../../components/BannerSection/BannerSection";
+import AboutSection from "../../../components/AboutSection/AboutSection";
 
 const Home = (props) => {
   return (
     <>
       <BannerSection />
+      <AboutSection />
     </>
   );
 };
