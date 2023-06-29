1dd7babbdcdb42cb752b1a15ddf65dbc21e75440
nihalkolidev <nihal.koli.dev@gmail.com>
2023-06-29 12:07:23 +0530

    Added Skills Grid item component for Skills

---
+++ src/components/Skills/GridItem/GridItem.js
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const GridItem = (props) => {
+  const {count ,skillName,colorText} =props;
+  return (
+    <>
+      <div className="grid" style={{background:`linear-gradient(180deg, ${colorText} 0%, rgba(31, 30, 28, 0.6) 67.59%)`}}> 
+        <div className="grid-inner">
+          <h3>
+            <span className="odometer" data-count={count}>
+            {count ?? '00'}
+            </span>
+            %
+          </h3>
+          <p style={{color:`${colorText}`}}>{skillName}</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+GridItem.propTypes = {};
+
+export default GridItem;
